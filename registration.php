37324600f78bc6cc6a0e696f167092f148804f5e
Jay Parmar <jay@meetanshi.com>
2025-12-11 10:44:39 +0530

    first commit

---
+++ registration.php
@@ -0,0 +1,4 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Meetanshi/hyva_werra', __DIR__);
---
+++ web/tailwind/tailwind.browser-jit-config.js
@@ -0,0 +1,10 @@
+const { spacing } = require('tailwindcss/defaultTheme');
+//const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    container: {
+      center: true
+    },
+  }
+}
